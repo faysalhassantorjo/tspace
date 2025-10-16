9a6f4bbfd07c976c5310afe280da6567b25b937a
torjo <faysalhassantorjo8@gmail.com>
2025-10-16 22:33:55 +0600

    Refactor static file settings to use os.path.join for compatibility

--- project/settings.py
+++ project/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # app-level static folder
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
