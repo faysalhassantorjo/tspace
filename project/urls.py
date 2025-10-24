74c2169215e3f73e8f6c3318ba13fba725120545
torjo <faysalhassantorjo8@gmail.com>
2025-10-24 15:36:43 +0600

    Integrate Django Allauth for user authentication and account management; update settings and URLs accordingly

--- project/urls.py
+++ project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('doc.urls')),
+    path('accounts/', include('allauth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
