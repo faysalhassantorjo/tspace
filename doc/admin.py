3f7c809e50892a3bd430b9c3fbef068330d5b7f4
torjo <faysalhassantorjo8@gmail.com>
2025-11-04 01:31:59 +0600

    Django Rest Framework

--- doc/admin.py
+++ doc/admin.py
@@ -4,7 +4,24 @@ from .models import Collection, Topic, Documentation, Project, Technology, Proje
 
 admin.site.register(Collection)
 admin.site.register(Topic)
-admin.site.register(Documentation)
+# admin.site.register(Documentation)
 admin.site.register(Project)
 admin.site.register(Technology)
 admin.site.register(ProjectImage)
+
+from django import forms
+class DocumentationForm(forms.ModelForm):
+    class Meta:
+        model = Documentation
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Order the Topic dropdown by created_at descending (newest first)
+        self.fields['topic'].queryset = Topic.objects.order_by('-created_at')
+        
+@admin.register(Documentation)
+class DocumentationAdmin(admin.ModelAdmin):
+    form = DocumentationForm
+    list_display = ('topic', 'created_at', 'updated_at')
+
